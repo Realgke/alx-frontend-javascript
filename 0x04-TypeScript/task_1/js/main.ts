5204b1cc501f5081a43be7183df7ec99642a65bc
Wanjohi Wanjiru <WanjohiWanjiru@gmail.com>
2025-09-15 12:42:48 +0300

    printTeacherFunction4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,3 +11,28 @@ type NameParams = {
 export function printTeacher({ firstName, lastName }: NameParams): string {
   return `${firstName}. ${lastName}`;
 }
+
+/* ---------- Interfaces for StudentClass ---------- */
+// Describes the constructor signature
+export interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+// Describes the instance (the methods/properties)
+export interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+/* ---------- Implementation ---------- */
+export class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
